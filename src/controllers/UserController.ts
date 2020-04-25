2dca6fb175cc74d445cb00188ada34da05f1e807
Felipe Lima <felipewikky@gmail.com>
2020-04-24 23:24:08 -0300

    Update UserController routes

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -18,13 +18,25 @@ class UserController {
 
   public signIn(req: Request, res: Response) {
     const {login, password} = req.body;
+
+    console.log(`${login} e ${password}`);
+
+    if (login == 'felipewikky' && password == '123') {
+      res.status(200).json({
+        status:200,
+        token: '25sd4sa56da1s'
+      });
+    } else {
+      res.status(404).json({
+        status:404,
+        message:'Credenciais não encontradas'
+      });
+    }
     
     // console.log(login);
     // console.log(password);
 
-    res.status(200).json({
-      token: '25sd4sa56da1s'
-    });
+    
   }
 }
 
