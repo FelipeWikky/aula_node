e26191511003a29747c025e967c15d8a161c584b
Felipe Lima <felipewikky@gmail.com>
2020-04-30 16:10:53 -0300

    Alterando variáveis do UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,7 +2,9 @@ import { Request, Response } from 'express';
 
 class UserController {
   public signUp(req: Request, res: Response) {
-    const { address, age, email, name, user:{password} } = req.body;
+    const { address, age, email, name, userPassword } = req.body;
+
+    console.log(req.body);
 
     res.status(200).json({
       token: '25sd4sa56da1s'
